105eb4de1bf68f9f74fc2307855c29bcb9c06e89
Vladyslav Nikishyn <nikivlad81@gmail.com>
2022-08-18 21:04:29 +0300

    [Strings] Second task has been corrected.

--- src/strings/Strings.php
+++ src/strings/Strings.php
@@ -4,6 +4,7 @@ namespace strings;
 
 class Strings implements StringsInterface
 {
+
     public function snakeCaseToCamelCase(string $input): string
     {
         return lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $input))));
@@ -11,22 +12,11 @@ class Strings implements StringsInterface
 
     public function mirrorMultibyteString(string $input): string
     {
-        $array = explode(" ", $input);
-
-        foreach($array as $value) {
-            $string = strrev(mb_convert_encoding($value, 'UTF-16BE', 'UTF-8'));
-            $new_array[] = mb_convert_encoding($string, 'UTF-8', 'UTF-16LE').' ';
-        }
-        return trim(implode($new_array));
+        return implode(' ', array_reverse(explode(' ', implode(array_reverse(mb_str_split($input))))));
     }
-
 
     public function getBrandName(string $noun): string
     {
-        if (mb_substr($noun, 0, 1) !== mb_substr($noun, -1, 1)) {
-            return "The " . ucfirst($noun);
-        } else {
-            return ucfirst($noun) . mb_substr($noun, 1);
-        }
+        return (mb_substr($noun, 0, 1) !== mb_substr($noun, -1, 1)) ?  "The " . ucfirst($noun) : ucfirst($noun) . mb_substr($noun, 1);
     }
 }
